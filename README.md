95ef1f06f04edb6c9dd866749b5b93702adbd9c2
Adam Pickering <adamkpickering@gmail.com>
2018-06-15 22:23:20 -0600

    Replaced ansible setup script with bash, because ansible was overkill, I had to install ansible, and a bash script is faster

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
-Installation
-------------
+# Installation
 
-First, install [ansible][1].
-
-Then, execute
-
-    ansible-pull -U https://github.com/adamkpickering/dotfiles.git -i hosts -e "username=[your username]" setup.yml
-
-Where [your username] is the name of your local user account (i.e. if my home directory is /home/adam my username is "adam"). Ansible will do the rest for you.
-
-[1]: http://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html
+```
+git clone https://github.com/adamkpickering/dotfiles.git
+dotfiles/install.sh
+```
---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+#----------------------------------------
+# vim
+
+# pathogen
+echo -n installing pathogen...
+mkdir -p ~/.vim
+mkdir -p ~/.vim/autoload
+mkdir -p ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo " done"
+
+# vim-sensible
+echo -n installing vim-sensible...
+rm -rf ~/.vim/bundle/vim-sensible
+git clone -q https://github.com/tpope/vim-sensible.git ~/.vim/bundle/vim-sensible
+echo " done"
+
+# vim-javascript
+echo -n installing vim-javascript...
+rm -rf ~/.vim/bundle/vim-javascript
+git clone -q https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript
+echo " done"
+
+# vim-jsx
+echo -n installing vim-jsx...
+rm -rf ~/.vim/bundle/vim-jsx
+git clone -q https://github.com/mxw/vim-jsx.git ~/.vim/bundle/vim-jsx
+echo " done"
+
+cp .vimrc ~/.vimrc
+
+#----------------------------------------
+# other
+
+#cp .gitconfig ~/.gitconfig
