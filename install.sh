3caaf4b1a523c1d85c024ba96d84fb74b7f3411a
Adam Pickering <adam.pickering@telus.com>
2019-08-23 17:13:53 -0600

    Put .vimrc into heredoc

--- install.sh
+++ install.sh
@@ -5,33 +5,51 @@
 # vim
 #--------------------------------------------------------------------------------
 
-## pathogen
-#echo -n installing pathogen...
-#mkdir -p ~/.vim
-#mkdir -p ~/.vim/autoload
-#mkdir -p ~/.vim/bundle
-#curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-#echo " done"
-#
-## vim-sensible
-#echo -n installing vim-sensible...
-#rm -rf ~/.vim/bundle/vim-sensible
-#git clone -q https://github.com/tpope/vim-sensible.git ~/.vim/bundle/vim-sensible
-#echo " done"
-#
-## vim-javascript
-#echo -n installing vim-javascript...
-#rm -rf ~/.vim/bundle/vim-javascript
-#git clone -q https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript
-#echo " done"
-#
-## vim-jsx
-#echo -n installing vim-jsx...
-#rm -rf ~/.vim/bundle/vim-jsx
-#git clone -q https://github.com/mxw/vim-jsx.git ~/.vim/bundle/vim-jsx
-#echo " done"
-#
-#cp .vimrc ~/.vimrc
+# pathogen
+echo -n 'installing pathogen...'
+mkdir -p ~/.vim
+mkdir -p ~/.vim/autoload
+mkdir -p ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo " done"
+
+# vim-sensible
+echo -n 'installing vim-sensible...'
+rm -rf ~/.vim/bundle/vim-sensible
+git clone -q https://github.com/tpope/vim-sensible.git ~/.vim/bundle/vim-sensible
+echo " done"
+
+# vim-javascript
+echo -n 'installing vim-javascript...'
+rm -rf ~/.vim/bundle/vim-javascript
+git clone -q https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript
+echo " done"
+
+# vim-jsx
+echo -n 'installing vim-jsx...'
+rm -rf ~/.vim/bundle/vim-jsx
+git clone -q https://github.com/mxw/vim-jsx.git ~/.vim/bundle/vim-jsx
+echo " done"
+
+cat << EOF > ~/.vimrc
+syntax on
+color elflord
+set expandtab
+set number
+set scrolloff=999
+
+autocmd FileType javascript.jsx setlocal shiftwidth=2 tabstop=2
+autocmd FileType json           setlocal shiftwidth=2 tabstop=2
+autocmd FileType yaml           setlocal shiftwidth=2 tabstop=2
+autocmd FileType markdown       setlocal shiftwidth=2 tabstop=2
+autocmd FileType html           setlocal shiftwidth=2 tabstop=2
+autocmd FileType htmldjango     setlocal shiftwidth=2 tabstop=2
+autocmd FileType python         setlocal shiftwidth=4 tabstop=4
+autocmd FileType sh             setlocal shiftwidth=4 tabstop=4
+
+execute pathogen#infect()
+filetype plugin indent on
+EOF
 
 
 #--------------------------------------------------------------------------------
