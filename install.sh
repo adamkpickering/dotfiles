d5e0ce90d59fb2cc5c03707e4a8218690c4471fb
Adam Pickering <adamkpickering@gmail.com>
2019-12-31 16:50:04 -0700

    Updated tab/space rules in .vimrc

--- install.sh
+++ install.sh
@@ -42,20 +42,28 @@ echo " done"
 cat << EOF > ~/.vimrc
 syntax on
 color elflord
-set expandtab
+set noexpandtab
 set number
 set scrolloff=999
 " other settings will mess with crontab -e
 set backupcopy=yes
 
-autocmd FileType javascript.jsx setlocal shiftwidth=2 tabstop=2
-autocmd FileType json           setlocal shiftwidth=2 tabstop=2
-autocmd FileType yaml           setlocal shiftwidth=2 tabstop=2
-autocmd FileType markdown       setlocal shiftwidth=2 tabstop=2
-autocmd FileType html           setlocal shiftwidth=2 tabstop=2
-autocmd FileType htmldjango     setlocal shiftwidth=2 tabstop=2
-autocmd FileType python         setlocal shiftwidth=4 tabstop=4
-autocmd FileType sh             setlocal shiftwidth=4 tabstop=4
+" expandtab/noexpandtab -> whether entered tabs are turned into spaces
+" shiftwidth -> how many columns text is indented with << and >>
+" tabstop -> the number of spaces a tab is represented by
+" softtabstop -> used for mixing tabs and spaces if softtabstop < tabstop
+"       and expandtab is not set
+
+autocmd FileType javascript.jsx setlocal shiftwidth=2 tabstop=2 noexpandtab
+autocmd FileType json           setlocal shiftwidth=2 tabstop=2 noexpandtab
+autocmd FileType yaml           setlocal shiftwidth=2 tabstop=2 expandtab
+autocmd FileType markdown       setlocal shiftwidth=2 tabstop=2 expandtab
+autocmd FileType html           setlocal shiftwidth=2 tabstop=2 noexpandtab
+autocmd FileType htmldjango     setlocal shiftwidth=2 tabstop=2 noexpandtab
+autocmd FileType python         setlocal shiftwidth=4 tabstop=4 expandtab
+autocmd FileType sh             setlocal shiftwidth=4 tabstop=4 noexpandtab
+autocmd FileType css            setlocal shiftwidth=4 tabstop=4 noexpandtab
+autocmd FileType sshconfig      setlocal shiftwidth=4 tabstop=4 noexpandtab
 
 execute pathogen#infect()
 filetype plugin indent on
