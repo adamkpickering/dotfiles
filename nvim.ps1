d57d0599862cc29e149f832aa05a7c4f43aad977
Adam Pickering <adamkpickering@gmail.com>
2022-05-05 09:50:18 -0700

    Add scripts for install on windows

---
+++ nvim.ps1
@@ -0,0 +1,4 @@
+#New-Item -Force $HOME\AppData\Local\nvim
+cp init.vim $HOME\AppData\Local\nvim\init.vim
+iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
+    ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
