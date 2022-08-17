4edcc7578abe9d65021768ab54b925a77ae3c58b
Adam Pickering <adamkpickering@gmail.com>
2022-08-17 17:16:39 -0600

    Tweak wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,13 +2,14 @@ local wezterm = require 'wezterm'
 local act = wezterm.action
 
 return {
+  default_prog = { 'nu' },
   keys = {
     { key = 't', mods = 'CTRL', action = act.SpawnTab 'CurrentPaneDomain' },
-    { key = 'F1', action = act.ActivateTab(0) },
-    { key = 'F2', action = act.ActivateTab(1) },
-    { key = 'F3', action = act.ActivateTab(2) },
-    { key = 'F4', action = act.ActivateTab(3) },
-    { key = 'F5', action = act.ActivateTab(4) },
+    { key = '1', mods = 'ALT', action = act.ActivateTab(0) },
+    { key = '2', mods = 'ALT', action = act.ActivateTab(1) },
+    { key = '3', mods = 'ALT', action = act.ActivateTab(2) },
+    { key = '4', mods = 'ALT', action = act.ActivateTab(3) },
+    { key = '5', mods = 'ALT', action = act.ActivateTab(4) },
     { key = 'PageDown', mods = 'CTRL', action = act.ActivateTabRelative(1) },
     { key = 'PageUp', mods = 'CTRL', action = act.ActivateTabRelative(-1) },
   },
